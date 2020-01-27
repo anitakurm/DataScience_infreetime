a70a96c5552e36a4bce25cc7d30c1dc7c04a4f25
anitakurm <anitakurm@gmail.com>
2020-01-27 15:49:47 +0100

    added twitter api notes (fake credentials)

--- dataimport.py
+++ dataimport.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Notes for different kinds of data import in Python
-Author: Anita Kurm; code is some solutions to DataCamp.com exercises
+Author: Anita Kurm; code is som solutions to DataCamp.com exercises by Hugo Bowne-Anderson
 """
 
 
@@ -153,3 +153,146 @@ a_tags = soup.find_all("a")
 # Print the URLs to the shell
 for link in a_tags:
     print(link.get('href'))
+
+
+
+
+###--------------------------------------###
+###-----------WORKING WITH APIs----------###
+###--------------------------------------###
+
+#------OMDB API (movie database)------#
+# Import requests package
+import requests
+
+# Assign URL to variable: url
+# query 'http://www.omdbapi.com' for the data corresponding to the movie The Social Network. 
+# The query string should have two arguments: apikey=72bc447a and t=the+social+network
+url = 'http://www.omdbapi.com/?apikey=72bc447a&t=the+social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Print the text of the response
+print(r.text)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
+
+
+
+#------Wikipedia API-------#
+# Import package
+import requests
+
+# Assign URL to variable: url
+url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=pizza"
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print the Wikipedia page extract
+pizza_extract = json_data['query']['pages']['24768']['extract']
+print(pizza_extract)
+
+
+#------Twittter API-------#
+# Import package
+import tweepy
+
+# Store OAuth authentication credentials in relevant variables (Fake tokens and keys)
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+#define StreamListener (code by Hugo Bowne-Anderson)
+class MyStreamListener(tweepy.StreamListener): 
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__() self.num_tweets = 0
+        self.file = open("tweets.txt", "w")
+    def on_status(self, status):
+        tweet = status._json self.file.write(json.dumps(tweet) + '\\n') tweet_list.append(status)
+        self.num_tweets += 1
+        if self.num_tweets < 100:
+            return True 
+        else:
+            return False 
+        self.file.close()
+
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create your Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(['clinton', 'trump', 'sanders', 'cruz'])
+
+
+
+# Import package
+import json
+
+# String of path to file: tweets_data_path
+tweets_data_path = "tweets.txt"
+
+# Initialize empty list to store tweets: tweets_data
+tweets_data= []
+
+# Open connection to file
+tweets_file = open(tweets_data_path, "r")
+
+# Read in tweets and store in list: tweets_data
+for line in tweets_file:
+    tweet = json.loads(line)
+    tweets_data.append(tweet)
+
+# Close connection to file
+tweets_file.close()
+
+# Print the keys of the first tweet dict
+print(tweets_data[0].keys())
+
+# Import package
+import pandas as pd
+
+# Build DataFrame of tweet texts and languages
+df = pd.DataFrame(tweets_data, columns=['text','lang'])
+
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text('trump', row['text'])
+    sanders += word_in_text('sanders', row['text'])
+    cruz += word_in_text('cruz', row['text'])
+
+# Import packages
+import matplotlib.pyplot as plt, seaborn as sns
+
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot the bar chart
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
